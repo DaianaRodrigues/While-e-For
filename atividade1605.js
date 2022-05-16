a37f2be03e391dbe50188e1fd46d0e87e23aae42
DaianaRodrigues <102737592+DaianaRodrigues@users.noreply.github.com>
2022-05-16 15:42:43 -0300

    Add files via upload

---
+++ atividade1605.js
@@ -0,0 +1,33 @@
+// Atividade - Daiana Rodrigues Nascimento
+
+// 1
+
+let a = 1
+while(a <= 100){
+    console.log('números = ' + a)
+    a = a + 1
+}
+
+// 2
+
+let b = 50
+while(b <= 100){
+    console.log('números = ' + b)
+    b = b + 1
+}
+
+// 3 
+
+let c = 10
+while(c >= 0){
+    console.log('contagem regressiva = ' + c)
+    c = c - 1
+}
+
+// 4
+
+let d = 0
+while(d <= 20){
+    console.log('Números par =' + d)
+    d = d + 2
+}
